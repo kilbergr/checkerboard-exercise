33d35ab87416e9c2299a9bee59b004288df082e0
kilbergr <rekilberg@gmail.com>
2015-05-13 18:12:44 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+var checkerboard = function(){
+	for (var i = 0; i <= 100; i++){
+	   if (i%2 === 0){
+			var newDiv = document.createElement("div");
+			newDiv.style.paddingBottom = "11.1%";
+			newDiv.style.backgroundColor = "red";
+			newDiv.style.width= "11.1%";
+			newDiv.style.float="left";
+        } else if (i%2 === 1){
+           	var newDiv = document.createElement("div");
+			newDiv.style.paddingBottom = "11.1%";
+			newDiv.style.backgroundColor = "black";
+			newDiv.style.width= "11.1%";
+			newDiv.style.float="left";
+        }
+		document.body.appendChild(newDiv);
+	}
+}
+checkerboard();
