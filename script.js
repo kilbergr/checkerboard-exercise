fbd1d2d851004a5bd7a4da14586320068cc4c897
kilbergr <rekilberg@gmail.com>
2015-05-13 20:43:44 -0700

    random colors version

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
+function randomColor(){
+	var randNum = Math.floor(Math.random()*255);
+	return randNum;
+	}
+
 var checkerboard = function(){
 	for (var i = 0; i <= 100; i++){
-	   if (i%2 === 0){
-			var newDiv = document.createElement("div");
-			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = "red";
-			newDiv.style.width= "11.1%";
-			newDiv.style.float="left";
-        } else if (i%2 === 1){
            	var newDiv = document.createElement("div");
 			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = "black";
 			newDiv.style.width= "11.1%";
-			newDiv.style.float="left";
-        }
-		document.body.appendChild(newDiv);
+			newDiv.style.float="left";	
+			newDiv.style.backgroundColor ="rgb(" + randomColor() + ", " + randomColor() + ", " + randomColor() + ")";
+			document.body.appendChild(newDiv);
 	}
 }
 checkerboard();
